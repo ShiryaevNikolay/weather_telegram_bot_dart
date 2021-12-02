bde74b0bee6ade2e6803778b794e967bae123d4d
Nikolay Shiryaev <deendeeps@list.ru>
2021-12-02 17:25:54 +0300

    создается файл с url

---
+++ telegram_bot/bin/service/api/weather_urls.dart
@@ -0,0 +1,5 @@
+/// Хранит пути для получения данных с сайта [https://openweathermap.org/]
+abstract class WeatherUrls {
+  static const base = 'https://api.openweathermap.org/data/2.5/';
+  static const currentWeather = 'weather';
+}
