39c1398c2c129a047a0f1a803e9d0da336afd470
Nikolay Shiryaev <deendeeps@list.ru>
2021-12-02 17:25:36 +0300

    создается класс апи

---
+++ telegram_bot/bin/service/api/weather_api.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart';
+import 'weather_urls.dart';
+
+/// Класс для работы с апи прогноза погоды
+class WeatherApi {
+  static const _apiKey = 'e3b61d77078ab0540790b2d2042b23c5';
+  static const _cityDefault = 'Voronezh';
+  static const _langDefault = 'ru';
+  static const _unitsDefault = 'metric';
+
+  // Future<Response> getWeather() async {
+    
+  // }
+}
