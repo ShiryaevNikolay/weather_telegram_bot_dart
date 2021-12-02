f080249b1ce441b786791f2cdb0efa03eb7a062e
Nikolay Shiryaev <deendeeps@list.ru>
2021-12-02 17:27:37 +0300

    обновляется бот

--- telegram_bot/bin/telegram_bot.dart
+++ telegram_bot/bin/telegram_bot.dart
@@ -2,14 +2,23 @@ import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
 
+import 'service/repository/weather_repository.dart';
+
+const token = '2146129739:AAEyZuPEuvRtDPvuKaiBrXoTls-K-0yU1fg';
+
 Future<void> main() async {
 
+  final repository = WeatherRepository();
+
   var telegram =
-      Telegram('2146129739:AAEyZuPEuvRtDPvuKaiBrXoTls-K-0yU1fg');
+      Telegram(token);
   var event = Event((await telegram.getMe()).username!);
 
   TeleDart(telegram, event)
     ..start()
     ..onMessage(keyword: 'Fight for freedom')
-        .listen((message) => message.reply('Stand with Hong Kong'));
+        .listen((message) => message.reply('Stand with Hong Kong'))
+    ..onCommand('start')
+        .listen((message) { })
+    ..onCommand('weather');
 }
